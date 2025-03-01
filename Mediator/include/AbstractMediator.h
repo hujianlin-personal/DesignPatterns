a138c3c7325e110b8f14cfa39ab2612a1fffed22
胡健林 <18811371812@163.com>
2025-03-01 14:48:38 +0800

    1.给CMakeLists.txt添加注释 2.给AbstractMediator.h添加AI注释

--- Mediator/include/AbstractMediator.h
+++ Mediator/include/AbstractMediator.h
@@ -4,7 +4,7 @@
  * @Author: hujianlin
  * @Date: 2025-01-26 19:45:51
  * @LastEditors: hujianlin
- * @LastEditTime: 2025-01-26 19:54:09
+ * @LastEditTime: 2025-02-28 22:52:32
  */
 
 #ifndef _ABSTRACT_MEDIATOR_H_
@@ -14,6 +14,13 @@
 
 #include "AbstractColleague.h"
 
+/**
+ * @class AbstractMediator
+ * @brief 抽象中介者类，定义了与各个同事对象之间的交互接口。
+ *
+ * 中介者模式通过引入一个中介者对象来减少对象之间的直接交互，从而降低耦合度。
+ * 具体的中介者类将实现该接口，并负责协调各个同事对象的行为。
+ */
 class AbstractMediator
 {
 public:
